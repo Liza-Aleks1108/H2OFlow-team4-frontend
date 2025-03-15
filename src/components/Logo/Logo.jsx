c7b24048a3424b65a39f4531026dec5f720dba7c
Dianka24336 <dianka24336@gmail.com>
2025-03-15 11:49:45 +0200

    add langbutton to logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import s from "./Logo.module.css";
+import LangButton from "../LangButton/LangButton.jsx";
 
 const Logo = () => {
   return (
-    <Link to="/" className={s.logoTitle}>
-      AquaTrack
-    </Link>
+    <div className={s.logo}>
+      <Link to="/" className={s.logoTitle}>
+        AquaTrack
+      </Link>
+      <LangButton />
+    </div>
   );
 };
 
