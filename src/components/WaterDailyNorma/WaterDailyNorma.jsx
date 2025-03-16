db96f10903a72131ad3b6fc79f76703f3b39deab
Dianka24336 <dianka24336@gmail.com>
2025-03-16 13:52:28 +0200

    translate

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import style from "./WaterDailyNorma.module.css";
 import { useTranslation } from "react-i18next";
 
-const WaterDailyNorma = ({ dailyNorm }) => {
+const WaterDailyNorma = () => {
   const { t } = useTranslation(); 
+  const user = useSelector(selectUser);
+  const dailyNorm = user?.dailyNorm / 1000 || 1.5;
   return (
     <div className={style.normContainer}>
       <h3 className={style.textH3}>{dailyNorm}{t("waterDailyNorma.l")}</h3>
