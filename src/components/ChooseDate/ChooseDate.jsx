ab7ea814f869022c479566d634421fe44bea26b6
IgorJS-Voloshyn <igorVoloshyn.js@gmail.com>
2025-03-16 11:58:05 +0200

    ComponentCosenDate

--- src/components/ChooseDate/ChooseDate.jsx
+++ src/components/ChooseDate/ChooseDate.jsx
@@ -1,9 +1,23 @@
+import { useTranslation } from "react-i18next";
 import css from "./ChooseDate.module.css";
 
-const ChooseDate = ({ chosenDate }) => {
+const ChooseDate = ({ selectedDate }) => {
+  const { t, i18n } = useTranslation();
+
+  const formatDate = (date) => {
+    const today = new Date();
+    if (date.toDateString() === today.toDateString()) {
+      return t("chooseDate.today");
+    }
+    return date.toLocaleDateString(i18n.language, {
+      day: "numeric",
+      month: "long",
+    });
+  };
+
   return (
     <div>
-      <h3 className={css.daily_info}>{chosenDate}</h3>
+      <h3 className={css.daily_info}>{formatDate(selectedDate)}</h3>
     </div>
   );
 };
