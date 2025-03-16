32aedc5c5d6a972a80aa3992204b086469a0cda0
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2025-03-16 15:29:30 +0200

    fix state and ready WaterForm func

--- src/redux/water/selectors.js
+++ src/redux/water/selectors.js
@@ -1,7 +1,3 @@
-// export const selectWaterEntries = (state) => state.user.userData.waterData;
-// export const selectIsLoading = (state) => state.user.userData.isLoading;
-// // export const selectError = (state) => state.user.error;
-
-export const selectEntries = (state) => state.water.water.entries;
-export const selectLoading = (state) => state.water.water.loading;
-export const selectError = (state) => state.water.water.error;
+export const selectEntries = (state) => state.water.entries;
+export const selectLoading = (state) => state.water.loading;
+export const selectError = (state) => state.water.error;
