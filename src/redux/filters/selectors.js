0152ac4fd0438b22426b923b40bf801d335a70c4
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-03-16 19:11:45 +0200

    delete filters

--- src/redux/filters/selectors.js
+++
@@ -1,3 +0,0 @@
-export const selectIsLoading = (state) => state.water.isLoading;
-export const selectDayWater = (state) => state.water.perDay;
-export const selectMonthWater = (state) => state.water.perMonth;
--- src/redux/water/selectors.js
+++ src/redux/water/selectors.js
@@ -1,3 +1,8 @@
 export const selectEntries = (state) => state.water.entries;
 export const selectLoading = (state) => state.water.loading;
 export const selectError = (state) => state.water.error;
+
+// from filters
+export const selectIsLoading = (state) => state.water.isLoading;
+export const selectDayWater = (state) => state.water.perDay;
+export const selectMonthWater = (state) => state.water.perMonth;
