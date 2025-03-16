64ae8e56cab4d0a32f9c6149f489f4b88997f55a
Svitlana Mykhailiuk <svetiklan@gmail.com>
2025-03-16 12:18:46 +0200

    MonthRedux
    MonthRedux

---
+++ src/redux/month/selectors.js
@@ -0,0 +1,5 @@
+export const selectMonthWater = (state) => state.monthData.monthData.oneMonth;
+export const selectIsLoadingMonth = (state) => state.monthData.isLoadingMonth;
+export const selectError = (state) => state.monthData.isErrorMonth;
+
+// export const selectActiveDate = (state) => state.water?.activeDate || null;
---
+++ src/redux/month/slice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { initialState } from "../initialState";
+import { getWaterMonth } from "./operations";
+
+const handlePending = (state) => {
+  state.isLoadingMonth = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoadingMonth = false;
+  state.error = action.payload;
+};
+
+const monthSlice = createSlice({
+  name: "month",
+  initialState,
+  reducers: {
+    updateActiveDate: (state, action) => {
+      state.activeDate = action.payload;
+    },
+    resetActiveDate: (state) => {
+      state.activeDate = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getWaterMonth.pending, handlePending)
+      .addCase(getWaterMonth.rejected, handleRejected)
+      .addCase(getWaterMonth.fulfilled, (state, { payload }) => {
+        state.isLoadingMonth = false;
+        state.isErrorMonth = null;
+        state.monthData = payload;
+      });
+  },
+});
+
+export default monthSlice.reducer;
